d3fbff44ff75ed0c209a2e41061fe626356c3cab
ogzhnozcn13 <o.ozcan.91@gmail.com>
2023-04-01 14:37:17 +0300

    navbar first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import AppRouter from './routers/AppRouter';
 
 function App() {
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores nihil minima aut veritatis magnam repudiandae odit, voluptatum odio dolorum! Iusto consequatur aliquid nesciunt quae, aspernatur possimus repellat esse illum debitis?</p>
-    </div>
+    <>
+      <AppRouter/>
+    </>
   );
 }
 
---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,20 @@
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import Dashboard from "../pages/Dashboard"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const AppRouter = () => {
+  return (
+    <div>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element= {<Dashboard/>}/>
+            </Routes>
+            <Footer/>
+        </Router>
+    </div>
+  )
+}
+
+export default AppRouter
